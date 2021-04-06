4e9652f6426f859afc62bf7eb0b429077aadc295
yarinTag <taguori5@gmail.com>
2021-04-06 12:48:56 +0300

    Get user details (Admin routes)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -185,3 +185,29 @@ exports.logOut = catchAsyncErr(async (req, res, next) => {
     })
 })
 
+
+//<--------------- Admin Routes --------------->
+
+//Get all users => /api/v1/admin/users
+exports.allUsers = catchAsyncErr(async (req,res,next) => {
+    const users = await User.find();
+
+    res.status(200).json ({
+        success: true,
+        users
+    })
+})
+
+//Get user details => /api/v1/admin/user/:id
+exports.getUserDetails = catchAsyncErr( async (req,res,next) => {
+    const user = await User.findById(req.params.id);
+
+    if(!user) {
+        return next(new ErrorHandler(`User does not exists with id: ${req.params.id} `));
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+})
