276798924ba16345cbe2ccdc66328f4c73b5cec6
NirAssaraf <assarafnir@gmail.com>
2021-04-07 21:59:17 +0300

    create angular app

---
+++ dashboard/src/app/header/navsidebar/navsidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navsidebar',
+  templateUrl: './navsidebar.component.html',
+  styleUrls: ['./navsidebar.component.css']
+})
+export class NavsidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
