3067e379de242a54691f02874e669946858b6a29
NirAssaraf <assarafnir@gmail.com>
2021-04-14 15:44:52 +0300

    create services

--- dashboard/src/app/users/users.component.ts
+++ dashboard/src/app/users/users.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { User } from 'models/user';
 import { UsersService } from '../services/users.service';
 import { CurrentUserService } from '../services/current-user.service';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
+@Injectable()
 export class UsersComponent {
-    
+ 
+
   users : User[] = [];  
 
   constructor(private usersService : UsersService, 
