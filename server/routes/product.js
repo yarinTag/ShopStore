5ecaf15e854fd4041d8159a1977008f82398e6af
NirAssaraf <assarafnir@gmail.com>
2021-04-16 22:28:08 +0300

    fixed edit and delete

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,13 +5,12 @@ const router = require('express').Router();
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, getAllProductApi, getProductsCategoryPrice } = require('../controllers/productControllers');
 const { isAuthenticated, authorizeRoles } = require('../utils/auth');
 
-router.get('/products', getProducts);
 
 router.get('/product/:id', getSingleProduct);
 
 router.post('/admin/product/new', isAuthenticated, authorizeRoles('admin'), newProduct);
 
-router.put('/admin/product/:id', isAuthenticated, authorizeRoles('admin'), updateProduct);
+// router.put('/admin/product/:id', isAuthenticated, authorizeRoles('admin'), updateProduct);
 
 router.get('/getAllProduct', getAllProductApi);
 
@@ -19,5 +18,9 @@ router.delete('/admin/product/:id', isAuthenticated, authorizeRoles('admin'), de
 
 router.get('/products/getcategoryprice', getProductsCategoryPrice)
 
+//anguler
+router.get('/products', getProducts);
+router.put('/product/edit/:id',updateProduct);
+router.delete('/product/delete/:id',deleteProduct);
 
 module.exports = router;
